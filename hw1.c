ddd1c7598b215bffda461440d9102d745a12e979
Joyce Cho <aejh82081@gmail.com>
2016-09-13 14:17:15 -0600

    modify hw1.c to perform reverse function

--- hw1.c
+++ hw1.c
@@ -1,5 +1,29 @@
-#include "hw1.h"
+//HW2/ Problem 2
 
-char reverse_string(char *str, int length){
-//insert the code
+#include <stdio.h>
+#include <string.h>
+//#include <hw1.h>
+
+void reverse_string(char *str){
+ char temp;
+ char* end;
+ end = str + strlen(str)-1;
+
+ while(end>str){
+   temp = *end;
+   *end = *str;
+   *str = temp;
+   end --;
+   str ++;
+ }
+}
+
+
+int main (int argc, const char *argv[]){
+
+ char string[] = "foobar";
+ reverse_string(string);
+ printf("%s\n", string);
+
+ return 1;
 }
